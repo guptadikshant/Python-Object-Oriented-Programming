c725f8e47ed52c778fec1d6be995076821e6eb81
Dikshant Gupta <guptadikshant99@gmail.com>
2023-03-24 21:13:26 +0530

    added some methods to class

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 class Item:
     """
     This class contains items details
-    """
+    """ 
+    pay_rate = 10
 
-    def calculate_total_price(self) -> None:
-        print("tested")
+    def __init__(self,name,price,quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.total = 0 
+
+    def calculate_total_price(self):
+        self.total = self.price * self.quantity
+        return self.total
+    
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
+
+    def __repr__(self):
+        return f"Item('{self.name}','{self.price}','{self.quantity}')"
 
 
 item1 = Item()
