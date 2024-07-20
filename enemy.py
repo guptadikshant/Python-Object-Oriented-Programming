95bc28261b45658a6dd6f02b929c8dd5d342d611
Dikshant Gupta <guptadikshant99@gmail.com>
2024-07-20 15:45:09 +0530

    private attribute

--- enemy.py
+++ enemy.py
@@ -1,19 +1,19 @@
 class Enemy:
 
     def __init__(self, type_of_enemy: str, health_points: int = 10 , attack_damage: int = 1) -> None:
-        self.type_of_enemy = type_of_enemy
+        self.__type_of_enemy = type_of_enemy # private attribute
         self.health_points = health_points
         self.attack_damage = attack_damage
         
 
     def talk(self):
-        print(f"I am a {self.type_of_enemy}. Be prepared to fight!")
+        print(f"I am a {self.__type_of_enemy}. Be prepared to fight!")
 
     def walk_farward(self):
-        print(f"{self.type_of_enemy} moves closer to you.")
+        print(f"{self.__type_of_enemy} moves closer to you.")
 
     def attack(self):
-        print(f"{self.type_of_enemy} attacks for {self.attack_damage} damage.")
+        print(f"{self.__type_of_enemy} attacks for {self.attack_damage} damage.")
 
 
 if __name__ == "__main__":
